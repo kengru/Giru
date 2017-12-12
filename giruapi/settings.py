4b9d551008bad326526a00f0afc97d8e5445cb5e
Kendry Grullon <kengrullon@gmail.com>
2017-12-12 13:16:25 -0400

    Created some models and migrated them to the database.

--- giruapi/settings.py
+++ giruapi/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'giru.apps.GiruConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Santo_Domingo'
 
 USE_I18N = True
 
